b7840949f6684b35751bb22b3d84df80e8ec736d
euqimor <romique@gmail.com>
2017-05-28 23:54:50 +0300

    End of the course

--- contoso/static/js/temp.js
+++ contoso/static/js/temp.js
@@ -1,13 +1,1 @@
-$(document).ready(function () {
-    manipulateDOM();
-});
 
-function manipulateDOM() {
-   var h1Headers = $('h1');
-   h1Headers.text('Huilo World');
-   var h3Headers = $('h3');
-   h3Headers.first().css('text-decoration', 'line-through');
-   h3Headers.css('color', 'red');
-   var lastH3 = $('h3:last');
-   lastH3.css('background-color', 'black');
-}
